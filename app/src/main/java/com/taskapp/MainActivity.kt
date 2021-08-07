dcc88e37e8959701d446e9ab03514571217d981f
nchan17 <nchan17@freeuni.edu.ge>
2021-08-07 20:59:27 +0400

    create login/signup layouts

--- app/src/main/java/com/taskapp/MainActivity.kt
+++ app/src/main/java/com/taskapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.taskapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.taskapp.databinding.ActivityMainBinding
+import com.taskapp.presentation.RegisterUser
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.registerTextView.setOnClickListener(this)
+
+        setContentView(binding.root)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.register_textView -> {
+                    startActivity(Intent(this, RegisterUser::class.java))
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/taskapp/presentation/RegisterUser.kt
@@ -0,0 +1,12 @@
+package com.taskapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taskapp.R
+
+class RegisterUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
